3de9854563b738161e782f88ab4cae4c14952044
Gianluca Carloni <91902479+gianlucarloni@users.noreply.github.com>
2023-04-04 18:36:04 +0200

    Update docker_run_distributed.sh
    clean version without old comments

--- docker_run_distributed.sh
+++ docker_run_distributed.sh
@@ -3,14 +3,6 @@
 NUM_GPUS=$(($1))
 NUM_CPUS=$((16*$NUM_GPUS))
 
-#nvidia-smi --query-gpu=memory.used --format=csv > tmp.txt \
-#&& tail -n +2 tmp.txt > tmp2.txt \
-#&& sed s/'\w*$'// tmp2.txt > tmp3.txt \
-#&& nl -w2 -s' ' tmp3.txt > tmp4.txt \
-#&& sort -k2 -n tmp4.txt > tmp5.txt \
-#&& awk '{print $1}' tmp5.txt > tmp6.txt \
-#&& head -$NUM_GPUS tmp6.txt > tmp7.txt \
-#&& cat tmp7.txt
 rm tmp*.txt
 
 nvidia-smi --query-gpu=memory.used,utilization.gpu --format=csv > tmp.txt \
@@ -19,7 +11,6 @@ nvidia-smi --query-gpu=memory.used,utilization.gpu --format=csv > tmp.txt \
 touch tmp3.txt
 while read -r line;
 do
-    #mystr=$(echo $line | tr -dc '0-9')
     IFS=',' read -ra my_array <<< "$line"
     mem=${my_array[0]}
     mem=$(echo $mem | tr -dc '0-9')
@@ -41,8 +32,6 @@ cat tmp3.txt \
 && head -$NUM_GPUS tmp6.txt > tmp7.txt \
 && cat tmp7.txt
 
-#################################################à
-
 arrVar=()
 c=1
 while read -r line;
@@ -55,10 +44,6 @@ do
         arrVar+="$(($line-1))";
     fi;
 done < tmp7.txt
-
-##
-#arrVar="3,4"
-##
 
 echo "The top $NUM_GPUS free GPUs are: $arrVar"
 arrVar2="device="$arrVar
@@ -74,9 +59,3 @@ docker run \
     --volume $PWD:$PWD \
     --workdir $PWD \
     gianlucacarloni/cond_gen:distributed_v0.1 $NUM_GPUS $arrVar
-
-#$@ \
-#train_conditionalDDPM.py $NUM_GPUS $arrVar
-    #gianlucacarloni/cond_gen:distributed_v0.1 $NUM_GPUS $arrVar
-
-#    --gpus  \
